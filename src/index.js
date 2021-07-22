8893a247892145cd3a67dd7b0ac857a6439d8d01
kaylashovlowsky <85142863+kaylashovlowsky@users.noreply.github.com>
2021-07-22 08:19:49 -0700

    Fixed bug with fahrenheit conversion

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 function weatherDisplay(response) {
   let temperature = Math.round(response.data.main.temp);
   let heading = document.querySelector("#temperature-display");
-  heading.innerHTML = `${temperature}°F`;
+  heading.innerHTML = `${temperature}`;
 }
 
 function enterCity(event) {
@@ -10,7 +10,7 @@ function enterCity(event) {
   let display = document.querySelector("h1");
 
   let apiKey = "632a5d0f15a7053d4f021e44e4d50ed0";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationSearch.value}&appid=${apiKey}&units=imperial`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationSearch.value}&appid=${apiKey}&units=metric`;
 
   display.innerHTML = `${locationSearch.value}`;
   axios.get(apiUrl).then(weatherDisplay);
@@ -20,11 +20,22 @@ function convertToFahrenheit(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature-display");
   let temperature = temperatureElement.innerHTML;
+  temperature = Number(temperature);
   temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
 }
 
+//function convertToCelsius(event) {
+//event.preventDefault();
+//let temperatureElement = document.querySelector("#temperature-display");
+//let temperature = temperatureElement.innerHTML;
+//temperatureElement.innerHTML = Math.round((temperature * 9) / 5 + 32);
+//}
+
 let fahernheitLink = document.querySelector("#fahrenheit-link");
 fahernheitLink.addEventListener("click", convertToFahrenheit);
+
+//let celsiusLink = document.querySelector("#celsius-link");
+//celsiusLink.addEventListener("click", convertToCelsius);
 
 let form = document.querySelector("#location-input");
 form.addEventListener("submit", enterCity);
